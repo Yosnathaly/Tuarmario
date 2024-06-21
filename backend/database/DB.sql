97398342e3822fe46abe0701498d6c71a1de8402
Shagooh <stgorodu@gmail.com>
2024-06-20 21:08:43 -0400

    adding routes

--- backend/database/DB.sql
+++ backend/database/DB.sql
@@ -9,6 +9,16 @@ CREATE TABLE usuarios (
   PRIMARY KEY (id)
 );
 
-INSERT INTO usuarios (
-  
-)
+CREATE TABLE productos (
+  id        SERIAL        NOT NULL,
+  descripcion     VARCHAR(255)   NOT NULL UNIQUE,
+  img  VARCHAR(255)   NOT NULL,
+  ingredients VARCHAR(255)   NOT NULL,
+  name VARCHAR(255)   NOT NULL,
+  price int NOT NULL,
+  cantidad int NOT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO productos (
+DEFAULT, "descripcion", "urlIMG", "ingredientes", "nombre", 100, 55)
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const env = require("dotenv").config();
 const cors = require("cors");
-const productos = require("../frontend/public/api_json/productos.json")
+const productos = require("./productos.json")
 const { pool } = require("./database/connection.js");
 const jwt = require("jsonwebtoken");
 const app = express();
@@ -16,5 +16,62 @@ app.use(express.json());
 
 app.get ("/productos", (req, res) => {
   res.status(200).send(productos)
-
 })
+
+// app.get ("/productos/:id", (req, res) => {
+//   res.status(200).send(productos)
+// })
+
+app.post("/productos", (req, res) => {
+  const producto = req.body
+  const { id } = producto
+  const existeUnproductoConEseId = productos.some(c => c.id == id)
+  if (existeUnproductoConEseId) res.status(400).send({ message: "Ya existe un producto con ese id" })
+  else {
+    productos.push(producto)
+      res.status(201).send(productos)
+  }
+});
+
+app.put("/productos/:id", (req, res) => {
+  const producto = req.body;
+  const { id } = req.params;
+  if (id != producto.id)
+      return res
+          .status(400)
+          .send({
+              message: "El id del parámetro no coincide con el id del producto recibido",
+          });
+
+  const productoIndexFound = productos.findIndex((p) => p.id == id);
+  if (productoIndexFound >= 0) {
+      productos[productoIndexFound] = producto;
+      res.send(productos);
+  } else {
+      res
+          .status(404)
+          .send({ message: "No se encontró ningún producto con ese id" });
+  }
+});
+
+app.delete("/productos/:id", (req, res) => {
+  const jwt = req.header("Authorization")
+  if (jwt) {
+      const { id } = req.params
+      const productoIndexFound = productos.findIndex(c => c.id == id)
+
+      if (productoIndexFound >= 0) {
+          productos.splice(productoIndexFound, 1)
+          console.log(productoIndexFound, productos)
+          res.send(productos)
+      } else {
+          res.status(404).send({ message: "No se encontró ningún producto con ese id" })
+      }
+
+  } else res.status(400).send({ message: "No recibió ningún token en las cabeceras" })
+});
+
+
+app.use("*", (req, res) => {
+  res.status(404).send({ message: "La ruta asdasdsque intenta consultar no existe" })
+});
